c891688d4dd3801f3398b177e57a0763dd655e80
Brighton <2018218409@student.unza.zm>
2023-07-20 14:46:57 +0200

    added dropdown button to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "../App.css";
+
 
  function Nav (){
 return( 
@@ -8,8 +8,15 @@ return(
     <li><a class="active" href="Home">Home</a></li>
     <li><a href="#news">Prices</a></li>
     <li><a href="Contact">Contact</a></li>
-    <li><a href="About">About</a></li>
-    </ul>
+    <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+  </ul>
 );
 
 }export default Nav;
